08d4ba68b96f579fe61c5c17b129921861bf33e3
Jenna Johnson <111776036+JohnsonJenna@users.noreply.github.com>
2023-03-23 21:07:54 +0000

    Thu, Mar 23, 2023, 2:07 PM -07:00

--- js/colorboard.js
+++ js/colorboard.js
@@ -4,5 +4,9 @@ const SQUARES = 500
 
 for (let i = 0; i < SQUARES; i++) {
     const square = document.createElement('div')
-    square.classList.add
+    square.classList.add('square')
+    container.appendChild(square)
+    square.addEventListener('mouseover', () => setColo(square))
 }
+
+function
