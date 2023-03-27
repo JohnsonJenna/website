4eec88a75ce8cbd73ec769fca475c528244a595b
Jenna Johnson <111776036+JohnsonJenna@users.noreply.github.com>
2023-03-27 21:51:57 +0000

    Mon, Mar 27, 2023, 2:51 PM -07:00

--- js/counter.js
+++ js/counter.js
@@ -6,7 +6,7 @@ counters.forEach(counter => {
     const updateCounter = () => {
         const target = +counter.getAttribute('data-target')
         const c = +counter.innerText
-        const increment = target /1000000000
+        const increment = target /500
 
         if (c < target) {
             counter.innerText = `${Math.ceil(c + increment)}`
--- js/typing.js
+++ js/typing.js
@@ -13,7 +13,6 @@ writeText()
 function writeText(){
     textEl.innerText = text.slice(0,idx)
     idx++
-
     if (idx > text.length){
         idx = 1
     }
