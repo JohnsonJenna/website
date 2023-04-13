9f9a7e2e0e5a14b34802ffbdb83a75f336aab0c3
Jenna Johnson <111776036+JohnsonJenna@users.noreply.github.com>
2023-04-13 21:07:18 +0000

    Thu, Apr 13, 2023, 2:07 PM -07:00

--- js/typing.js
+++ js/typing.js
@@ -4,8 +4,8 @@ const text = "We Love Programming!!!"
 let idx = 1
 let speed = 300 / speedEl.value
 
-speedEl.addEventListener('input', (e) => {
-    speed = 300 / e.target.value
+speedEl.addEventListener('input', (element) => {
+    speed = 300 / element.target.value
 })
 
 writeText()
